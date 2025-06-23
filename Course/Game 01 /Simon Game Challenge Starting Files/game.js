cc25216c7cb6151e643581924e498af8e2f18149
Rakindu Niwunhella <rakindu15@gmail.com>
2025-06-23 20:12:51 +0530

    jd

--- Course/Game 01 /Simon Game Challenge Starting Files/game.js
+++ Course/Game 01 /Simon Game Challenge Starting Files/game.js
@@ -3,8 +3,11 @@ let buttonColours =["red","blue","green","yellow"];
 
 function nextSequence(){
     return Math.floor(Math.random() *4)} 
+    let randomChosenColour = buttonColours[nextSequence];
+    gamePattern.push(randomChosenColour);
+    return randomChosenColour;
     
 let num = nextSequence();
 console.log(num);
+console.log(gamePattern);
 
-let randomChosenColour = buttonColours[nextSequence];
