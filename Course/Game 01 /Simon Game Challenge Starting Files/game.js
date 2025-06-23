6e32e9bc6c7e8dcdba08fc2c82737f461bae8c62
Rakindu Niwunhella <rakindu15@gmail.com>
2025-06-23 19:48:37 +0530

    dji

---
+++ Course/Game 01 /Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,10 @@
+let gamePattern = [];
+let buttonColours =["red","blue","green","yellow"];
+
+function nextSequence(){
+    return Math.floor(Math.random() *4)} 
+    
+let num = nextSequence();
+console.log(num);
+
+let randomChosenColour = buttonColours[nextSequence];
