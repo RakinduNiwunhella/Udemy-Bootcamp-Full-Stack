4acaff3f20b37c2c6d2cfbc9d296adc73dcfc0b3
Rakindu Niwunhella <rakindu15@gmail.com>
2025-09-24 00:55:00 +0530

    up

---
+++ Course/EJS/index.js
@@ -0,0 +1,16 @@
+import exress from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res) =>{
+    res.render("index.ejs", {
+        dayType: "a Weekday",
+        advice: "it's time to work harder", 
+    });
+});
+
+
+app.listen(port, () => {
+    console.log('Server running on port ${port}');
+});
